cad460d68d1ce64e43c669e13294c4c96c7a856e
= <robinsonweng100@gmail.com>
2024-09-13 15:56:33 +0800

    add test case for given not capitalized name in json value

--- webapp/api/view/order_format_converter.py
+++ webapp/api/view/order_format_converter.py
@@ -25,6 +25,9 @@ class OrderFormatConverterView(viewsets.ViewSet):
         if self.contain_non_english(data_serializer["name"].value):
             raise ValidationError("Name contains Non-English characters")
 
+        if self.not_capicalize(data_serializer["name"].value):
+            raise ValidationError("Name is not capitalized")
+
         return Response(
             data={},
             status=status.HTTP_201_CREATED,
@@ -42,3 +45,44 @@ class OrderFormatConverterView(viewsets.ViewSet):
                 return True
 
         return False
+
+    def not_capicalize(self, value: str):
+        """
+            this function assume that the value already check by
+            contain_non_english function
+
+            the c & ~(32) is a way to force ascii char into upper
+            case using bitwise operation, e.g.
+
+            ascii_a = ord('a') # 97
+
+            97 & ~(32)
+
+            97     = 01100001
+            ~(32)  = 11011111
+            ------- and ---------
+            65     = 01000001
+
+            ascii_a = ord('A') # 65
+
+            65 & ~(32)
+
+            65     = 01000001
+            ~(32)  = 11011111
+            ------- and ---------
+            65     = 01000001
+
+        """
+
+        secperated_letter = value.split(' ')
+
+        for letter in secperated_letter:
+            origin_letter = letter[0]
+            masked_letter = chr(ord(letter[0]) & ~(32))
+
+            if origin_letter != masked_letter:
+                return True
+
+        return False
+
+
